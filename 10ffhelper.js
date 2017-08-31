8e0a7da9318e29ea5200bee389d784964766eb27
govind cs <govindsomalal7@gmail.com>
2017-08-31 18:55:22 +0530

    added readme and updated script to newer version

--- 10ffhelper.js
+++ 10ffhelper.js
@@ -1,15 +1,26 @@
+// paste the script in the console , a new input field should appear, use that to set the permissible number of
+// errors. When you make that many errors , the page should auto restart.
+
+// keep your hands on the home row!
 var errorssofar = 0;
-var allowed = 3;
+var limiter = document.createElement("div");
 
-
+limiter.innerHTML = '' + '<div class="text-center col-sm-6 col-sm-offset-3">' +
+    '<input type="text" class="limiterfield"  id= "limitField" value="3" ><input type="button" class="limiterfield"  id= "lim" value="SET" onclick = "btnclick()">';
+document.getElementsByClassName('control-label')[0].appendChild(limiter);
+var allowed = document.getElementById("limitField").value;
 $("#inputfield").keydown(function(a) {
     if (a.keyCode == 32) {
         if ($("#inputfield").val() != $(".highlight").text()) {
             errorssofar++;
         }
-        if (errorssofar > allowed) {
+        if (errorssofar > parseInt(allowed)) {
             $("#reload-btn").click();
-
+            errorssofar = 0;
         }
     }
 });
+
+function btnclick() {
+    allowed = document.getElementById('limitField').value;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# An auto restarter for 10fastfingers
+
+[10fastfingers](https://10fastfingers.com/) is the most popular online typing test. This script adds a much needed auto restart function to the site. When practising it is often desirable to just restart the test if you make too many mistakes so people just end up refresing the page or clicking the soft refresh button with their mouse.
+
+This script will let you auto restart the test if a set amount of mistakes is made , that way your fingers can stay where they are.
